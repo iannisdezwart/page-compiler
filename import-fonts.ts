591d1632a5099a6e39433153e391f7c0af77988a
iannisdezwart <iannis__@hotmail.com>
2021-07-01 14:59:46 +0200

    🛠 Import Google Fonts Function Now Supports Unsorted Arrays As Input

--- import-fonts.ts
+++ import-fonts.ts
@@ -6,7 +6,15 @@ interface FontStyle {
 }
 
 export const importGoogleFont = async (fontFamily: string, styles: FontStyle[]) => {
-	const stylesString = styles.map(style => `${ style.italic ? 1 : 0 },${ style.weight }`).join(';')
+	const stylesString = styles
+		.sort((a, b) => {
+			if (a.italic && !b.italic) return 1
+			if (b.italic && !a.italic) return -1
+			return a.weight - b.weight
+		})
+		.map(style => `${ style.italic ? 1 : 0 },${ style.weight }`)
+		.join(';')
+
 	const url = `https://fonts.googleapis.com/css2?family=${ fontFamily }:ital,wght@${ stylesString }&display=swap`
 
 	return /* html */ `
