fca51cff7af7ef0b6145d361d4011ffac4e27807
iannisdezwart <iannis__@hotmail.com>
2021-07-14 18:01:32 +0200

    🛠 Fixed Bug In Include Resources Function

--- include-resources.ts
+++ include-resources.ts
@@ -2,12 +2,15 @@ import * as fs from 'fs'
 import { FilePath } from './util'
 import { resolve as resolvePath } from 'path'
 
-export const includeResource = (resourcePath: string) => {
-	const path = new FilePath(resourcePath)
+export const includeResource = (srcPath: string) => {
+	const path = new FilePath(srcPath)
+	const destPath = `root/res/${ path.fullFilename() }`
 
 	if (!fs.existsSync('root/res')) {
 		fs.mkdirSync('root/res', { recursive: true })
 	}
 
-	fs.symlinkSync(resolvePath(resourcePath), `root/res/${ path.fullFilename() }`)
+	if (!fs.existsSync(destPath)) {
+		fs.symlinkSync(resolvePath(srcPath), destPath)
+	}
 }
